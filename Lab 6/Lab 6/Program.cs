2cac095985440c305b0b7802b0ba3e8769eed541
Carmen Benavente <cbenavente@miuandes.cl>
2020-05-19 12:36:49 -0400

    Exceptions de FileNotFound

--- Lab 6/Lab 6/Program.cs
+++ Lab 6/Lab 6/Program.cs
@@ -17,7 +17,93 @@ namespace Lab_6
                 int option = int.Parse(Console.ReadLine());
                 if (option == 1)
                 {
-                    empresas = LoadE();
+                    try
+                    {
+                        empresas = LoadE();
+                    }
+                    catch (FileNotFoundException exp)
+                    {
+                        Console.WriteLine("No se encontró el archivo, se creará uno nuevo.");
+                        Console.WriteLine(exp.Message) ;
+                        Console.WriteLine("Escriba los datos de la empresa");
+                        Console.Write("Nombre: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Rut: ");
+                        string rut = Console.ReadLine();
+                        Empresa e = new Empresa(name, rut);
+                        Persona Edepartamento = new Persona("A", "A", "19.639.828-5", "Encargado Departamento");
+                        Persona Eseccion = new Persona("B", "B", "19.689.587-8", "Encargado Sección");
+                        Persona Ebloque1 = new Persona("C", "C", "20.881.887-4", "Encargado Bloque");
+                        Persona Ebloque2 = new Persona("D", "D", "11.833.498-K", "Encargado Bloque");
+                        Persona persona1 = new Persona("E", "E", "12.232.498-4", "Trabajador");
+                        Persona persona2 = new Persona("F", "F", "8.107.074-3", "Trabajador");
+                        Persona persona3 = new Persona("G", "G", "19.685.827-1", "Trabajador");
+                        Persona persona4 = new Persona("H", "H", "20.463.389-4", "Trabajador");
+                        Bloque bloque1 = new Bloque();
+                        Bloque bloque2 = new Bloque();
+                        Seccion seccion = new Seccion();
+                        Departamento departamento = new Departamento();
+                        bloque1.Personas.Add(persona1);
+                        bloque1.Personas.Add(persona2);
+                        bloque2.Personas.Add(persona3);
+                        bloque2.Personas.Add(persona4);
+                        bloque1.Encargado = Ebloque1;
+                        bloque2.Encargado = Ebloque2;
+                        seccion.Encargado = Eseccion;
+                        departamento.Encargado = Edepartamento;
+                        e.Divisiones.Add(bloque1);
+                        e.Divisiones.Add(bloque2);
+                        e.Divisiones.Add(seccion);
+                        e.Divisiones.Add(departamento);
+                        empresas.Add(e);
+                        SaveE(empresas);
+                    }
+                    catch (System.Runtime.Serialization.SerializationException exp1)
+                    {
+                        Console.WriteLine("El archivo está vacio, se creará uno nuevo.");
+                        Console.WriteLine(exp1.Message);
+                        Console.WriteLine("Escriba los datos de la empresa");
+                        Console.Write("Nombre: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Rut: ");
+                        string rut = Console.ReadLine();
+                        Empresa e = new Empresa(name, rut);
+                        Persona Edepartamento = new Persona("A", "A", "19.639.828-5", "Encargado Departamento");
+                        Persona Eseccion = new Persona("B", "B", "19.689.587-8", "Encargado Sección");
+                        Persona Ebloque1 = new Persona("C", "C", "20.881.887-4", "Encargado Bloque");
+                        Persona Ebloque2 = new Persona("D", "D", "11.833.498-K", "Encargado Bloque");
+                        Persona persona1 = new Persona("E", "E", "12.232.498-4", "Trabajador");
+                        Persona persona2 = new Persona("F", "F", "8.107.074-3", "Trabajador");
+                        Persona persona3 = new Persona("G", "G", "19.685.827-1", "Trabajador");
+                        Persona persona4 = new Persona("H", "H", "20.463.389-4", "Trabajador");
+                        Bloque bloque1 = new Bloque();
+                        Bloque bloque2 = new Bloque();
+                        Seccion seccion = new Seccion();
+                        Departamento departamento = new Departamento();
+                        bloque1.Personas.Add(persona1);
+                        bloque1.Personas.Add(persona2);
+                        bloque2.Personas.Add(persona3);
+                        bloque2.Personas.Add(persona4);
+                        bloque1.Encargado = Ebloque1;
+                        bloque2.Encargado = Ebloque2;
+                        seccion.Encargado = Eseccion;
+                        departamento.Encargado = Edepartamento;
+                        e.Divisiones.Add(bloque1);
+                        e.Divisiones.Add(bloque2);
+                        e.Divisiones.Add(seccion);
+                        e.Divisiones.Add(departamento);
+                        empresas.Add(e);
+                        try
+                        {
+                            SaveE(empresas);
+                        }
+                        catch (System.IO.IOException exp2)
+                        {
+                            Console.WriteLine("No se logró guardar correctamente.");
+                            Console.WriteLine(exp2.Message);
+                        }
+                    }
+                    
                 }
                 else if (option == 2)
                 {
@@ -52,14 +138,17 @@ namespace Lab_6
                     e.Divisiones.Add(seccion);
                     e.Divisiones.Add(departamento);
                     empresas.Add(e);
-                    SaveE(empresas);
+                    try
+                    {
+                        SaveE(empresas);
+                    }
+                    catch (System.IO.IOException exp2)
+                    {
+                        Console.WriteLine("No se logró guardar correctamente.");
+                        Console.WriteLine(exp2.Message);
+                    }
                 }
             }
-        }
-
-        private static void SerializationException()
-        {
-            Console.WriteLine("No se ha encontrado el archivo, vuelva a iniciar el programa indicando la opción 2");
         }
 
         static private void SaveE(List<Empresa> empresas)
